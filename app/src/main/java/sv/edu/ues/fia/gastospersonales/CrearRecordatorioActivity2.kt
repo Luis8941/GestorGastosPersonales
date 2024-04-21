5848ce02fde296c905c66b53929ce8c5157993fe
Luis8941 <55610146+Luis8941@users.noreply.github.com>
2024-04-20 23:33:06 -0600

    Creando la vista de listado de recordatorio

---
+++ app/src/main/java/sv/edu/ues/fia/gastospersonales/CrearRecordatorioActivity2.kt
@@ -0,0 +1,11 @@
+package sv.edu.ues.fia.gastospersonales
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CrearRecordatorioActivity2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_crear_recordatorio2)
+    }
+}
